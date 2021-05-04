bcbd015c9607fbd8c4b9f5b8a29c9e8c16173f38
Eric Wong <ericwong12@live.com>
2021-05-04 14:43:50 -0600

    Feat: Boilerplate
    First commit

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar-title>Crypto Wallet Tracker</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <img class="google-icon" src="search.png" alt="google-icon" />
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.google-icon {
+  width: 30px;
+}
+</style>
