949824b9ae3452246fb789398a0e81aed2d85602
Eric Wong <ericwong12@live.com>
2021-05-04 16:36:15 -0600

    Feat: Google Sign in
    Sign in option pulls data, now needs to display data

---
+++ components/Auth/GoogleSignIn.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="google-portal">
+    <button @click.prevent="googleSignup">
+      <img class="google-icon" src="search.png" alt="Google Icon" />
+    </button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app'
+export default {
+  name: 'GoogleSignIn',
+  methods: {
+    googleSignup() {
+      var provider = new firebase.auth.GoogleAuthProvider()
+      this.$fire.auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          /** @type {firebase.auth.OAuthCredential} */
+          var credential = result.credential
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = credential.accessToken
+          console.log(token)
+          // The signed-in user info.
+          var user = result.user
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          var errorCode = error.code
+          var errorMessage = error.message
+          // The email of the user's account used.
+          var email = error.email
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential
+          // ...
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+.google-icon {
+  width: 35px;
+}
+</style>
