01edfa6818d66a7239a3cf9435476a6ed0b2d8bf
Eric Wong <ericwong12@live.com>
2021-05-06 16:30:31 -0600

    Feat: Navbar
    Add Navbar boilerplate, needs some functionality overall

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,9 +1,49 @@
 <template>
-  <v-app-bar :clipped-left="clipped" fixed>
-    <v-app-bar-title>Crypto Wallet Tracker</v-app-bar-title>
-    <v-spacer></v-spacer>
-    <GoogleSignIn />
-  </v-app-bar>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Crypto Wallet Tracker</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <GoogleSignIn v-if="user === null" />
+            <b-navbar-nav v-if="user">
+              <b-nav-text> Welcome, {{ user.displayName }} </b-nav-text>
+            </b-navbar-nav>
+            <b-dropdown-item @click="logout" v-if="user">
+              Logout
+            </b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -12,6 +52,11 @@ export default {
   components: {
     GoogleSignIn,
   },
+  computed: {
+    user() {
+      return this.$store.state.auth.user
+    },
+  },
   data() {
     return {
       clipped: false,
@@ -19,11 +64,25 @@ export default {
       fixed: false,
     }
   },
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logoutUser')
+    },
+  },
 }
 </script>
 
 <style scoped>
-.google-icon {
-  width: 30px;
+.navbar-text {
+  display: block;
+  width: 100%;
+  padding: 0.25rem 1.5rem;
+  clear: both;
+  font-weight: 400;
+  color: #212529;
+  text-align: inherit;
+  white-space: nowrap;
+  background-color: transparent;
+  border: 0;
 }
 </style>
