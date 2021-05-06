10abddad3cd234adac7368f71b6c11d8601eaa4b
Eric Wong <ericwong12@live.com>
2021-05-06 16:33:23 -0600

    Feat: mutation (auth)
    Add listener to auth status

--- store/auth/mutations.js
+++ store/auth/mutations.js
@@ -1,14 +1,15 @@
 const mutations = {
-    ON_AUTH_STATE_CHANGED_MUTATION:(state, {authUser, claims}) =>{
-        if(!authUser){
-          state.user = null
-        }else{
-          const { email, uid } = authUser
-          const admin = claims.admin
-          state.user = { email, uid, admin }
-          
-        }
+  ON_AUTH_STATE_CHANGED_MUTATION:(state, {authUser}) =>{
+      if(!authUser){
+        state.user = null
+      }else{
+        const { email, uid, displayName} = authUser
+        state.user = { email, uid, displayName}
+        
       }
+    },
+
+    
 }
 
 export default mutations
