971f50c0eb29f86db217ca1466162add647f0945
Eric Wong <ericwong12@live.com>
2021-05-07 01:50:26 -0600

    Feat: default (layout)
    Page layout changed
    Now resembles coinbases layout
    Not mobile friendly just yet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>
-    <Navbar />
-    <Nuxt />
+  <div class="layout">
+    <Sidebar class="sidebar" />
+    <div class="content">
+      <Navbar />
+      <Nuxt />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Navbar from '~/components/UI/Navigation/Navbar'
+import Sidebar from '~/components/UI/Navigation/Sidebar'
+export default {
+  components: {
+    Navbar,
+    Sidebar,
+  },
+}
 </script>
 
-<style>
+<style scoped>
+.layout {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  background: hsla(210, 10%, 23%, 1);
+  background: linear-gradient(
+    315deg,
+    hsla(210, 10%, 23%, 0.7) 0%,
+    hsla(210, 10%, 23%, 1) 100%
+  );
+
+  background: -moz-linear-gradient(
+    315deg,
+    hsla(210, 10%, 23%, 0.7) 0%,
+    hsla(210, 10%, 23%, 1) 100%
+  );
+
+  background: -webkit-linear-gradient(
+    315deg,
+    hsla(210, 10%, 23%, 0.7) 0%,
+    hsla(210, 10%, 23%, 1) 100%
+  );
+
+  filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#343A40", endColorstr="#343A40", GradientType=1 );
+}
 </style>
