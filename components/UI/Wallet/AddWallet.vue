a572d3667e9c40099d4e998fa59cfd115c65d236
Eric Wong <ericwong12@live.com>
2021-05-07 12:49:13 -0600

    Feat: AddWallet Component
    User's way to add a wallet to their account

---
+++ components/UI/Wallet/AddWallet.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-modal :id="modal" title="BootstrapVue">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    modal: String,
+  },
+}
+</script>
+
+<style>
+</style>
