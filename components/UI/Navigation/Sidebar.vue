08854b219e0cc2f33256da99374d7e71bdccb528
Eric Wong <ericwong12@live.com>
2021-05-07 01:49:21 -0600

    Feat: Sidebar
    Sidebar is a navigation seperate from the navbar.
    Handles user's actions, creating wallets or editing

---
+++ components/UI/Navigation/Sidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sidebar">
+    <div class="logo">
+      <div class="logo-name">Crypto Wallet Tracker</div>
+    </div>
+    <div class="buttons">
+      <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.sidebar {
+  background: #343a40;
+  color: white;
+  border-right: 1px solid white;
+}
+.logo {
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  height: 8%;
+}
+</style>
