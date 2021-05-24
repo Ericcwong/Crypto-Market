88e452ec5184d1fec8b77a5e6d9fa64fcc656bdf
Eric Wong <ericwong12@live.com>
2021-05-23 20:22:33 -0600

    Feat: nuxt.config.js
    Add nuxt composition api

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,23 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel:"stylesheet",
-        href:"https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap",
-      }
-    ]
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "~/assets/style.css"
-  ],
+  css: ['~/assets/style.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -36,7 +33,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://dev.bootstrap-vue.org/docs
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/composition-api/module',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -50,23 +48,23 @@ export default {
       '@nuxtjs/firebase',
       {
         config: {
-          apiKey: "AIzaSyDF5FFYa2DEtG-eW1_8LhPS5BqN74-oA_g",
-          authDomain: "crypto-wallet-tracker.firebaseapp.com",
-          projectId: "crypto-wallet-tracker",
-          storageBucket: "crypto-wallet-tracker.appspot.com",
-          messagingSenderId: "1007376571117",
-          appId: "1:1007376571117:web:fa7b4586abdc6a78964ae4",
-          measurementId: "G-6C1ELDNCJL"
+          apiKey: 'AIzaSyDF5FFYa2DEtG-eW1_8LhPS5BqN74-oA_g',
+          authDomain: 'crypto-wallet-tracker.firebaseapp.com',
+          projectId: 'crypto-wallet-tracker',
+          storageBucket: 'crypto-wallet-tracker.appspot.com',
+          messagingSenderId: '1007376571117',
+          appId: '1:1007376571117:web:fa7b4586abdc6a78964ae4',
+          measurementId: 'G-6C1ELDNCJL',
         },
         services: {
-          auth:{
+          auth: {
             initialize: {
               onAuthStateChangedMutation: 'auth/ON_AUTH_STATE_CHANGED_MUTATION',
             },
-          }
-        }
-      }
-    ]
+          },
+        },
+      },
+    ],
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -75,17 +73,14 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
-
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    babel:{
-      plugins: [
-        ['@babel/plugin-proposal-private-methods', { loose: true }]
-      ]
-    }
-  }
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
 }
