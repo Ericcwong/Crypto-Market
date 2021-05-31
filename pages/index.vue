f9d4a7e0a5c3245d0a1085857a6a1b98fb7cbf90
Eric Wong <ericwong12@live.com>
2021-05-30 20:47:57 -0600

    Feat: index.vue
    Removed old static data.
    Now populates with fresh data.
    Everything is dynamic and will add options later to move to next 100 cryptos

--- pages/index.vue
+++ pages/index.vue
@@ -1,114 +1,61 @@
 <template>
-  <section class="landing-page">
-    <div class="title">
-      Crypto wallet
-      <div class="icons">
-        <img
-          v-for="(icon, index) in icons"
-          :class="{ crypto: index }"
-          :src="icon.icon"
-          :key="icon.index"
-          :alt="index"
-        />
+  <div class="container">
+    <section class="landing-page">
+      <div class="title">
+        Crypto wallet
+        <Table :cryptos="state.cryptos" />
       </div>
-      <div
-        class="prices"
-        v-for="crypto in state.cryptos"
-        :key="crypto.cryptoName"
-      >
-        <span class="crypto-name">
-          {{ crypto.cryptoName }}
-        </span>
-        <span>${{ crypto.cryptoPrice }}</span>
-      </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
 import { reactive, ref, onMounted } from '@nuxtjs/composition-api'
 import axios from 'axios'
+import Table from '~/components/UI/Market/Table'
 export default {
-  components: {},
+  data() {
+    return {
+      greater: {
+        color: '#2E8B57',
+      },
+      less: {
+        color: 'red',
+      },
+    }
+  },
+  components: { Table },
   setup() {
     const state = reactive({
       cryptos: [],
     })
-    // Saves space in the template area
-    const icons = ref([
-      {
-        icon:
-          'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@07fd63a0b662ed99c8ad870ee9227b8ef5e11630/svg/color/btc.svg',
-        ref: 'bitcoin',
-      },
-      {
-        icon:
-          'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@07fd63a0b662ed99c8ad870ee9227b8ef5e11630/svg/color/eth.svg',
-        ref: 'ethereum',
-      },
-      {
-        icon:
-          'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@07fd63a0b662ed99c8ad870ee9227b8ef5e11630/svg/color/usdt.svg',
-        ref: 'tether',
-      },
-      {
-        icon:
-          'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@07fd63a0b662ed99c8ad870ee9227b8ef5e11630/svg/color/doge.svg',
-        ref: 'dogecoin',
-      },
-      {
-        icon:
-          'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@07fd63a0b662ed99c8ad870ee9227b8ef5e11630/svg/color/ada.svg',
-        ref: 'cardano',
-      },
-    ])
-
     const getPriceAPI = async () => {
-      let data = icons.value.forEach((el) => {
-        const cryptoName = el.ref
-        axios
-          .get(
-            `https://api.coingecko.com/api/v3/simple/price?ids=${cryptoName}&vs_currencies=usd`
-          )
-          .then((res) => {
-            let cryptoPrice = res.data[cryptoName].usd
-            state.cryptos.push({ cryptoName, cryptoPrice })
-          })
-      })
+      axios
+        .get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false'
+        )
+        .then((response) => {
+          console.log(response)
+          response.data.forEach((element) => state.cryptos.push(element))
+        })
     }
     onMounted(getPriceAPI)
-    return { icons, getPriceAPI, state }
+    return { getPriceAPI, state }
   },
 }
 </script>
 
 <style scoped>
 .landing-page {
-  display: grid;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
   color: white;
-  height: 100vh;
+  min-height: 100vh;
+  height: 100%;
 }
 
 .title {
   font-size: 3rem;
 }
-.icons {
-  display: flex;
-  justify-content: space-evenly;
-}
-.icon {
-  width: 5rem;
-  margin-left: 20px;
-  margin-right: 20px;
-  animation-name: slideInIcons;
-  animation-duration: 1s;
-}
-.crypto-name {
-  text-transform: capitalize;
-}
+
 /* Keyframes */
 @keyframes slideInIcons {
   from {
