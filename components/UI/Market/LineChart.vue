feadc67f1ae779cbb251084e3c560cca62d7909b
Eric Wong <ericwong12@live.com>
2021-06-01 16:16:27 -0600

    Fix: LineChart.vue
    Moved back to .vue component
    Now renders data accordingly

---
+++ components/UI/Market/LineChart.vue
@@ -0,0 +1,23 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+export default {
+  setup() {},
+  extends: Line,
+  mixins: [reactiveProp],
+  props: {
+    chartData: {
+      type: Array,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  async mounted() {
+    console.log(this.chartData)
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
