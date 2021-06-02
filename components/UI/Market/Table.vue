0114118a5e34d1b74b64d8f8f6987ec3cf360625
Eric Wong <ericwong12@live.com>
2021-06-02 17:22:41 -0600

    Fix: Table.vue
    Change props passing information
    Passing an array of 7 day changes instead of calling another API

--- components/UI/Market/Table.vue
+++ components/UI/Market/Table.vue
@@ -9,7 +9,7 @@
       <th>Market Cap</th>
       <th>Last 7 Days</th>
     </tr>
-    <tr v-for="crypto in cryptos" :key="crypto.id">
+    <tr v-for="crypto in state.cryptos" :key="crypto.id">
       <td class="ranking">
         {{ crypto.market_cap_rank }}
       </td>
@@ -33,7 +33,7 @@
       <td
         class="7-day-percentage"
         :style="[
-          crypto.market_cap_change_percentage_24h >= 0
+          crypto.price_change_percentage_7d_in_currency >= 0
             ? { color: '#00FF7F' }
             : { color: '#FF4500' },
         ]"
@@ -42,7 +42,7 @@
       </td>
       <td class="market-cap">{{ crypto.market_cap.toLocaleString() }}</td>
       <td class="chart">
-        <ChartContainer :crypto="crypto.id" />
+        <ChartContainer :crypto="crypto.sparkline_in_7d.price" />
       </td>
     </tr>
   </table>
@@ -56,8 +56,21 @@ export default {
   components: {
     ChartContainer,
   },
-  props: {
-    cryptos: Array,
+  setup() {
+    const state = reactive({
+      cryptos: [],
+    })
+    const getPriceAPI = async () => {
+      await axios
+        .get(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C7d'
+        )
+        .then((response) => {
+          response.data.forEach((element) => state.cryptos.push(element))
+        })
+    }
+    onMounted(getPriceAPI)
+    return { getPriceAPI, state }
   },
 }
 </script>
