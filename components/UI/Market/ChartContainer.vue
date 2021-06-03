0a8c51bf9b0d3393a3bdf165d6a374887e6d10ec
Eric Wong <ericwong12@live.com>
2021-06-02 19:04:34 -0600

    Feat: ChartContainer.vue
    Now configured to display the 7 day charts

--- components/UI/Market/ChartContainer.vue
+++ components/UI/Market/ChartContainer.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <line-chart
-      :chartData="state.dataCollections"
+      style="width: 100%; height: 10vh"
+      :data="crypto"
+      :chartColor="colorChoice"
       :options="state.options"
     ></line-chart>
   </div>
@@ -10,39 +12,51 @@
 <script>
 import LineChart from './LineChart.vue'
 import { onMounted, reactive } from '@nuxtjs/composition-api'
-import axios from 'axios'
 export default {
   components: { LineChart },
   props: {
-    crypto: Array,
-    default: null,
+    crypto: { type: Array, default: null },
+    chartColor: { type: String, default: null }, //Changes the chart color depending on the 7 day percentage number
   },
   setup(props) {
     const state = reactive({
       loaded: false,
-      dataCollections: [],
-      data: [],
-      // options: {
-      //   plugins: {
-      //     legend: {
-      //       display: false,
-      //       labels: {
-      //         color: 'rgb(255, 99, 132)',
-      //       },
-      //       title: { display: false },
-      // },
-      // },
-      // },
+      options: {
+        legend: {
+          display: false,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        showTooltips: false,
+        elements: {
+          point: {
+            radius: 0,
+          },
+        },
+        scales: {
+          xAxes: [
+            {
+              display: false, //this will remove all the x-axis grid lines
+            },
+          ],
+          yAxes: [
+            {
+              display: false, //this will remove all the y-axis grid lines
+            },
+          ],
+        },
+      },
     })
-    const loadDate = async () => {
-      let prices = props.crypto
-      console.log(prices)
-      state.dataCollection = prices
-    }
-    onMounted(loadDate)
-    return { state }
+    console.log(props.chartColor)
+    const colorChoice = parseInt(props.chartColor) >= 0 ? '#00FF7F' : '#FF4500'
+    console.log(colorChoice)
+    return { state, colorChoice }
   },
 }
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  height: 100%;
+}
+</style>
