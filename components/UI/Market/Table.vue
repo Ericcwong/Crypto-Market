3de350c0132eba75b096d8fab390c133518f5cef
Eric Wong <ericwong12@live.com>
2021-06-05 16:19:38 -0600

    Feat: Table.vue
    Data now loads when the user reaches the bottom of the page.
    Thus speeds of the loading process

--- components/UI/Market/Table.vue
+++ components/UI/Market/Table.vue
@@ -1,82 +1,103 @@
 <template>
-  <table class="sortable">
-    <tr>
-      <th>Ranking</th>
-      <th>Name</th>
-      <th>Price</th>
-      <th>24h %</th>
-      <th>7d %</th>
-      <th>Market Cap</th>
-      <th>Last 7 Days</th>
-    </tr>
-    <tr v-for="crypto in state.cryptos" :key="crypto.id">
-      <td class="ranking">
-        {{ crypto.market_cap_rank }}
-      </td>
-      <td class="name" style="text-transform: capitalize; text-align: left">
-        <img class="icon" :src="crypto.image" :alt="crypto.id" />
-        {{ crypto.id }}
-        <span style="text-transform: uppercase">{{ crypto.symbol }}</span>
-      </td>
-      <td class="current-price">
-        ${{ crypto.current_price.toLocaleString() }}
-      </td>
-      <td
-        class="24h-price-change-%"
-        :style="[
-          crypto.price_change_percentage_24h >= 0
-            ? { color: '#00FF7F' }
-            : { color: '#FF4500' },
-        ]"
-      >
-        {{ crypto.price_change_percentage_24h.toFixed(2) }}%
-      </td>
-      <td
-        class="7-day-percentage"
-        :style="[
-          crypto.price_change_percentage_7d_in_currency >= 0
-            ? { color: '#00FF7F' }
-            : { color: '#FF4500' },
-        ]"
-      >
-        {{ crypto.price_change_percentage_7d_in_currency.toFixed(2) }}%
-      </td>
-      <td class="market-cap">{{ crypto.market_cap.toLocaleString() }}</td>
-      <td class="chart" style="margin: -1px">
-        <ChartContainer
-          :crypto="crypto.sparkline_in_7d.price"
-          :chartColor="crypto.price_change_percentage_7d_in_currency.toFixed(2)"
-        />
-      </td>
-    </tr>
-  </table>
+  <div class="">
+    <table class="sortable">
+      <tr>
+        <th>Ranking</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>24h %</th>
+        <th>7d %</th>
+        <th>Market Cap</th>
+        <th>Last 7 Days</th>
+      </tr>
+      <tr v-for="crypto in state.cryptos" :key="crypto.id">
+        <td class="ranking">
+          {{ crypto.market_cap_rank }}
+        </td>
+        <td class="name" style="text-transform: capitalize; text-align: left">
+          <img class="icon" :src="crypto.image" :alt="crypto.id" />
+          {{ crypto.id }}
+          <span style="text-transform: uppercase">{{ crypto.symbol }}</span>
+        </td>
+        <td class="current-price">
+          ${{ crypto.current_price.toLocaleString() }}
+        </td>
+        <td
+          class="24h-price-change-%"
+          :style="[
+            crypto.price_change_percentage_24h >= 0
+              ? { color: '#00FF7F' }
+              : { color: '#FF4500' },
+          ]"
+        >
+          {{ crypto.price_change_percentage_24h.toFixed(2) }}%
+        </td>
+        <td
+          class="7-day-percentage"
+          :style="[
+            crypto.price_change_percentage_7d_in_currency >= 0
+              ? { color: '#00FF7F' }
+              : { color: '#FF4500' },
+          ]"
+        >
+          {{ crypto.price_change_percentage_7d_in_currency.toFixed(2) }}%
+        </td>
+        <td class="market-cap">{{ crypto.market_cap.toLocaleString() }}</td>
+        <td class="chart" style="margin: -1px">
+          <ChartContainer
+            :crypto="crypto.sparkline_in_7d.price"
+            :chartColor="
+              crypto.price_change_percentage_7d_in_currency.toFixed(2)
+            "
+          />
+        </td>
+      </tr>
+      <div
+        v-if="state.cryptos.length"
+        v-observe-visibility="handleScrolledToBottom"
+      ></div>
+    </table>
+    <v-pagination v-model="state.page" :length="6"></v-pagination>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
-import { onBeforeMount, reactive, toRefs } from '@nuxtjs/composition-api'
-
+import { onBeforeMount, reactive } from '@nuxtjs/composition-api'
+import ChartContainer from './ChartContainer'
 export default {
   components: {
-    ChartContainer: () => import('./ChartContainer'),
+    ChartContainer,
   },
   setup() {
     const state = reactive({
       cryptos: [],
+      loaded: 20,
+      page: 1,
     })
-    const getPriceAPI = () => {
-      axios
-        .get(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C7d'
-        )
-        .then((response) => {
-          console.log(response)
-          response.data.forEach((element) => state.cryptos.push(element))
-        })
+    const getPriceAPI = async () => {
+      let response = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${state.loaded}&page=${state.page}&sparkline=true&price_change_percentage=1h%2C7d`
+      )
+      let data = await response.data
+      let uniqueCrypto = await [...new Set(response.data)]
+      console.log(uniqueCrypto)
+      state.cryptos = []
+      state.cryptos.push(...uniqueCrypto)
     }
     onBeforeMount(getPriceAPI)
-
-    return { getPriceAPI, state }
+    function handleScrolledToBottom(isVisible) {
+      if (!isVisible) {
+        return
+      }
+      if (state.loaded !== 100) {
+        state.loaded = state.loaded + 20
+        getPriceAPI()
+      } else {
+        return
+      }
+    }
+    return { getPriceAPI, state, handleScrolledToBottom }
   },
 }
 </script>
