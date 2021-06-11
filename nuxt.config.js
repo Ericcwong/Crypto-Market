4f6f0cae89816c59c4d267026d301a313ae7afa3
Eric Wong <ericwong12@live.com>
2021-06-11 16:49:19 -0600

    Fix: nuxt.config.js
    Prepped for dynamic meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  ssr: true,
+  ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: 'Crypto Market %s',
-    title: 'crypto-market',
+    title: '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
