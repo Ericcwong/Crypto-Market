5b522b7b9cb2e4731db0a74d1d10e31a951de5cf
Eric Wong <ericwong12@live.com>
2021-06-15 16:34:07 -0600

    Feat: LineChart.vue
    Vertical line that follows the user mouse hover

--- components/UI/Market/LineChart.vue
+++ components/UI/Market/LineChart.vue
@@ -1,5 +1,6 @@
 <script>
 import { Line } from 'vue-chartjs'
+
 export default {
   extends: Line,
   props: {
@@ -16,13 +17,43 @@ export default {
     },
   },
   async mounted() {
+    //Handles the vertical line that appears when the user hovers over the graph
+    this.$refs.canvas.getContext('2d')
+    // new Chart(ctx, config);
+    var originalLineDraw = Chart.controllers.line.prototype.draw
+    Chart.helpers.extend(Chart.controllers.line.prototype, {
+      draw: function () {
+        originalLineDraw.apply(this, arguments)
+
+        var chart = this.chart
+        var ctx = chart.chart.ctx
+
+        if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+          var activePoint = this.chart.tooltip._active[0]
+          var ctx = this.chart.ctx
+          var x = activePoint.tooltipPosition().x
+          var topY = this.chart.scales['y-axis-0'].top
+          var bottomY = this.chart.scales['y-axis-0'].bottom
+
+          // draw line
+          ctx.save()
+          ctx.beginPath()
+          ctx.moveTo(x, topY)
+          ctx.lineTo(x, bottomY)
+          ctx.lineWidth = 0.5
+          ctx.strokeStyle = '#eeeeee'
+          ctx.stroke()
+          ctx.restore()
+        }
+      },
+    })
     this.renderChart(
       {
         labels: this.data,
         datasets: [
           {
             data: this.data,
-            // borderColor: this.chartColor,
+            borderColor: 'lightblue',
             fill: false,
           },
         ],
