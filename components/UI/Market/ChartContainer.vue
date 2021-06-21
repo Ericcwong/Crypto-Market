8a88c87440b53b7761fde8c96f1588076467d11a
Eric Wong <ericwong12@live.com>
2021-06-21 16:20:24 -0600

    Fix: ChartContainer.vue
    Moved _crypto.vue chart information to ChartContainer.vue
    Moved it to seperate and organize the files

--- components/UI/Market/ChartContainer.vue
+++ components/UI/Market/ChartContainer.vue
@@ -1,29 +1,143 @@
 <template>
   <div class="line-chart-container">
-    <line-chart
-      :chartData="state.chartData"
-      :options="state.options"
-    ></line-chart>
+    <span class="chart-title">{{ state.cryptoName }}</span>
+    <div v-if="state.dataLoaded !== false">
+      <line-chart
+        :chartData="state.chartData"
+        :options="state.options"
+      ></line-chart>
+    </div>
+
+    <v-card-text>
+      <v-chip
+        class="mr-2"
+        ref="1d"
+        @click="getSparklineDay({ day: 1, chartTimeOption: '1 Day' })"
+      >
+        <v-icon left>mdi-alarm-check </v-icon>
+        1 Day
+      </v-chip>
+      <v-chip
+        class="mr-2"
+        ref="1d"
+        @click="getSparklineDay({ day: 7, chartTimeOption: '7 Days' })"
+      >
+        <v-icon left>mdi-alarm-check </v-icon>
+        7 Day
+      </v-chip>
+      <v-chip
+        class="mr-2"
+        ref="1d"
+        @click="getSparklineDay({ day: 30, chartTimeOption: '30 Days' })"
+      >
+        <v-icon left>mdi-alarm-check </v-icon>
+        1 Month
+      </v-chip>
+    </v-card-text>
   </div>
 </template>
 
 <script>
 import LineChart from './LineChart.vue'
-import { toRefs, reactive } from '@nuxtjs/composition-api'
+import { toRefs, reactive, onMounted } from '@nuxtjs/composition-api'
+import axios from 'axios'
 export default {
   components: { LineChart },
   props: {
-    date: { type: Array },
-    price: { type: Array },
-    chartColor: { type: Number, default: null }, //Changes the chart color depending on the 7 day percentage number
+    cryptoName: { type: String },
   },
 
   setup(props) {
-    let date = props.date
-    console.log(date)
-    let price = props.price
+    //Assigning props
+    let cryptoName = props.cryptoName
+    // Handles the mouse hover over the graph. Formatting the date information to U.S. Standards.
+    const dateConverter = (data) => {
+      const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ]
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+      ]
+      const today = new Date(data)
+      let hours = today.getHours()
+      let minutes = today.getMinutes()
+      let ampm = hours >= 12 ? 'pm' : 'am'
+      hours = hours % 12
+      hours = hours ? hours : 12 // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0' + minutes : minutes
+      let dayName = days[today.getDay()]
+      let monthName = months[today.getMonth() + 1]
+      let date = `
+      ${dayName}${', '}${monthName} ${today.getDate()}th, ${today.getFullYear()} ${hours}:${minutes}${ampm} `
+      return date
+    }
+    const getSparklineDay = async (data) => {
+      state.chartData.labels = []
+      state.chartData.datasets[0].data = []
+      state.dataLoaded = false
+      console.log(data)
+      const sparklineData = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/${cryptoName.toLowerCase()}/market_chart?vs_currency=usd&days=${
+          data.day
+        }`
+      )
+      switch (data.chartTimeOption) {
+        case '1 Day':
+          console.log('1 Day')
+          sparklineData.data.prices.map((el) => {
+            state.chartData.labels.push(dateConverter(el[0]))
+            if (el[1] > 1) {
+              state.chartData.datasets[0].data.push(el[1].toFixed(2))
+            } else {
+              state.chartData.datasets[0].data.push(el[1].toFixed(3))
+            }
+          })
+          break
+        case '7 Days':
+          sparklineData.data.prices.map((el) => {
+            state.chartData.labels.push(dateConverter(el[0]))
+            if (el[1] > 1) {
+              state.chartData.datasets[0].data.push(el[1].toFixed(2))
+            } else {
+              state.chartData.datasets[0].data.push(el[1].toFixed(3))
+            }
+          })
+          break
+        case '30 Days':
+          sparklineData.data.prices.map((el) => {
+            state.chartData.labels.push(new Date(el[0]).toLocaleDateString())
+            if (el[1] > 1) {
+              state.chartData.datasets[0].data.push(el[1].toFixed(2))
+            } else {
+              state.chartData.datasets[0].data.push(el[1].toFixed(3))
+            }
+          })
+          break
+      }
+
+      console.log(state.chartData.datasets[0].data)
+      state.dataLoaded = true
+    }
     const state = reactive({
-      loaded: false,
+      dataLoaded: false,
+      // Chart Options
       options: {
         //Display the name of the graph
         legend: {
@@ -41,28 +155,33 @@ export default {
             radius: 1,
           },
         },
-        // scales: {
-        //   xAxes: [
-        //     {
-        //       display: false, //this will remove all the x-axis grid lines
-        //     },
-        //   ],
-        // },
+        scales: {
+          xAxes: [
+            {
+              title: {
+                text: 'hello there',
+              },
+              display: false,
+            },
+          ],
+        },
       },
       chartData: {
-        labels: date,
+        labels: [],
         datasets: [
           {
             label: 'Price: $',
-            data: price,
+            data: [],
             borderColor: 'lightblue',
             fill: false,
           },
         ],
       },
     })
-
-    return { state }
+    onMounted(() => {
+      getSparklineDay({ day: 1, chartTimeOption: '1 Day' })
+    })
+    return { state, getSparklineDay }
   },
 }
 </script>
