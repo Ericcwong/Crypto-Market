04894642d0c5f2d052886af4d9767db962d9a916
Eric Wong <ericwong12@live.com>
2021-06-22 15:51:45 -0600

    Feat: Search.vue
    Search option, but it needs a way to display choices

--- components/UI/Navigation/NavBarComponent/Search.vue
+++ components/UI/Navigation/NavBarComponent/Search.vue
@@ -1,13 +1,35 @@
 <template>
   <b-nav-form>
-    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-    <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+    <b-form-input
+      size="sm"
+      class="mr-sm-2"
+      placeholder="Search "
+      v-model="name"
+    ></b-form-input>
+    <b-button
+      size="sm"
+      class="my-2 my-sm-0"
+      type="submit"
+      @click.prevent="search"
+      >Search</b-button
+    >
   </b-nav-form>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      name: '',
+    }
+  },
+  methods: {
+    search() {
+      let name = this.name.toLowerCase().split(' ').join('')
+      this.$router.push(`/${name}`)
+    },
+  },
+}
 </script>
 
-<style>
-</style>
+<style></style>
