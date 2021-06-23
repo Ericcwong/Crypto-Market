5903279fd307039415e5d59a42498fc9498b3b09
Eric Wong <ericwong12@live.com>
2021-06-23 15:26:32 -0600

    Feat: _crypto.vue
    Adding more information about the crypto market

--- pages/_crypto/index.vue
+++ pages/_crypto/index.vue
@@ -19,13 +19,34 @@
           state.crypto.market_data.current_price.usd
         }}</span>
         <div class="crypto-information">
-          <span class="ranking">Rank: {{ state.crypto.coingecko_rank }}</span>
+          <span class="ranking"
+            >Market cap rank: #{{ state.crypto.market_cap_rank }}
+          </span>
+          <span
+            >Market Cap: ${{
+              state.crypto.market_data.market_cap.usd.toLocaleString()
+            }}
+          </span>
+          <span class="ath"
+            >ATH: ${{ state.crypto.market_data.ath.usd.toFixed(2) }}
+          </span>
+          <span class="24h-high"
+            >24 Hour High: ${{ state.crypto.market_data.high_24h.usd }}
+          </span>
+          <span class="24h-low"
+            >24 Hour Low: ${{ state.crypto.market_data.low_24h.usd }}
+          </span>
+          <span class="circulating-supply"
+            >Circulating Supply:
+            {{ state.crypto.market_data.circulating_supply }}</span
+          >
         </div>
       </div>
       <div class="middle-card-container">
         <ChartContainer :cryptoName="state.cryptoName" />
       </div>
       <div class="bottom-card-container">
+        <h4>Description:</h4>
         <p class="description" v-html="state.cryptoDescription" v-linkified></p>
       </div>
     </v-card>
@@ -70,6 +91,7 @@ export default {
       state.crypto = response.data
       state.cryptoName = response.data.id
       state.cryptoDescription = response.data.description.en
+      console.log(response.data)
     }
 
     onBeforeMount(getCryptoAPI)
@@ -89,7 +111,9 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
-.top-card-container > .crypto-information,
+.top-card-container > .crypto-information {
+  grid-column: 1 / span 2;
+}
 span {
   padding: 16px;
 }
@@ -102,5 +126,8 @@ span {
 }
 .price {
   text-align: right;
+}
+.bottom-card-container {
+  margin: 5% 15% 0 15%;
 }
 </style>
