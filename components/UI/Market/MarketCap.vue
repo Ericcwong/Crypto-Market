7479f519dca3948c1209e34631a606c07d15460d
Eric Wong <ericwong12@live.com>
2021-06-30 15:57:46 -0600

    Feat: MarketCap.vue
    Change from a long number value to displaying only:
    Trillion, hundred billion, and ten billions
    
    Then some rewording to clarify some details

--- components/UI/Market/MarketCap.vue
+++ components/UI/Market/MarketCap.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="market-cap-container" v-if="state.market !== null">
     <span class="market-cap">
-      Today's Cryptocurrency Market Price is: ${{
-        parseFloat(
-          state.market.total_market_cap.usd.toFixed(2)
-        ).toLocaleString()
-      }}
+      Today's Cryptocurrency Market Cap is: {{ state.marketCap }}
       <span
         class="market-up"
         v-if="state.market.market_cap_change_percentage_24h_usd > 0.0"
@@ -19,7 +15,7 @@
     </span>
     <div class="market-cap-bottom-container">
       <span class="coins"
-        >Coins: {{ state.market.active_cryptocurrencies }}</span
+        >Total Coins: {{ state.market.active_cryptocurrencies }}</span
       >
 
       <span class="24h-volume">
@@ -29,7 +25,7 @@
       </span>
 
       <span class="bitcoin-dominance">
-        Dominance BTC {{ state.market.market_cap_percentage.btc.toFixed(1) }}%
+        BTC Dominance {{ state.market.market_cap_percentage.btc.toFixed(1) }}%
       </span>
     </div>
   </div>
@@ -42,11 +38,18 @@ export default {
   setup() {
     let state = reactive({
       market: null,
+      marketCap: 0,
     })
     const getMarketData = async () => {
       const APIdata = await axios.get('https://api.coingecko.com/api/v3/global')
       state.market = await APIdata.data.data
       console.log(APIdata.data.data)
+      let number = APIdata.data.data.total_market_cap.usd
+      let trillions = String(number).charAt(0)
+      let hundredBillions = String(number).charAt(1)
+      let tenBillions = String(number).charAt(2)
+      const marketCap = `${trillions}.${hundredBillions}${tenBillions} Trillion`
+      state.marketCap = marketCap
     }
     onMounted(getMarketData)
     return { state }
@@ -64,10 +67,17 @@ export default {
 .market-cap {
   font-size: 1.75rem;
 }
+.market-cap-container {
+  border: 1px solid darkgray;
+  border-radius: 5px;
+  box-shadow: 10px 10px 5px #888;
+  margin-bottom: 3%;
+  padding: 3%;
+}
 .market-cap-bottom-container {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
-  border-bottom: 1px solid darkgray;
+  /* border-bottom: 1px solid darkgray; */
 }
 </style>
