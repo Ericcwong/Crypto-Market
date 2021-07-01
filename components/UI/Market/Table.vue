82f8b7616548b36affaa3902592e893f42916f49
Eric Wong <ericwong12@live.com>
2021-07-01 16:36:14 -0600

    Fix: Table.vue
    Moved table into mobile and desktop component
    Making it easier to maintain layouts

--- components/UI/Market/Table.vue
+++ components/UI/Market/Table.vue
@@ -8,80 +8,10 @@
         >{{ title.title }}</span
       >
     </div>
-    <v-card
-      class="desktop-layout"
-      elevation="4"
-      outlined
-      tile
-      v-for="(crypto, index) in state.cryptos"
-      :key="index"
-    >
-      <div class="crypto-data">
-        <div class="crypto-rank">
-          <v-card-subtitle class="mobile-title">Rank:</v-card-subtitle>
-          <v-card-title>{{ crypto.market_cap_rank }}</v-card-title>
-        </div>
-        <div class="crypto-name">
-          <v-card-subtitle class="mobile-title">Name:</v-card-subtitle>
-          <Nuxt-link :to="crypto.id">
-            <v-card-title style="color: white">
-              <img
-                style="width: 2rem"
-                :src="crypto.image"
-                :alt="crypto.id + ' icon'"
-              />&nbsp; {{ crypto.name }} &nbsp;
-              <small> {{ crypto.symbol.toUpperCase() }}</small>
-            </v-card-title>
-          </Nuxt-link>
-        </div>
-        <div class="crypto-price">
-          <v-card-subtitle class="mobile-title">Price:</v-card-subtitle>
-          <v-card-title v-if="crypto.current_price > 0.01"
-            >${{ parseFloat(crypto.current_price.toFixed(3)).toLocaleString() }}
-          </v-card-title>
-          <v-card-title v-else>${{ crypto.current_price }}</v-card-title>
-        </div>
-
-        <div class="crypto-24h">
-          <v-card-subtitle class="mobile-title">24h %</v-card-subtitle>
-          <v-card-title
-            class="twenty-four-hour-percentage"
-            :style="[
-              crypto.price_change_percentage_24h >= 0.0
-                ? { color: '#00FF7F' }
-                : { color: '#FF4500' },
-            ]"
-            >{{ crypto.price_change_percentage_24h.toFixed(2) }}%
-          </v-card-title>
-        </div>
-        <div class="crypto-7d">
-          <v-card-subtitle class="mobile-title">7d %</v-card-subtitle>
-          <v-card-title
-            class="seven-day-percentage"
-            :style="[
-              crypto.price_change_percentage_7d_in_currency >= 0.0
-                ? { color: '#00FF7F' }
-                : { color: '#FF4500' },
-            ]"
-            >{{ crypto.price_change_percentage_7d_in_currency.toFixed(2) }}%
-          </v-card-title>
-        </div>
-        <div class="crypto-market-cap">
-          <v-card-subtitle class="mobile-title">Market Cap:</v-card-subtitle>
-          <v-card-title>${{ crypto.market_cap.toLocaleString() }}</v-card-title>
-        </div>
-        <div class="crypto-chart">
-          <v-card-subtitle class="mobile-title">Last 7 days</v-card-subtitle>
-          <v-sparkline
-            :value="crypto.sparkline_in_7d.price"
-            :color="redOrGreen(crypto.price_change_percentage_7d_in_currency)"
-            :auto-draw-duration="3000"
-            line-width="2"
-            auto-draw
-          ></v-sparkline>
-        </div>
-      </div>
-    </v-card>
+    <div class="desktop-view">
+      <Desktop-Table :cryptos="state.cryptos" />
+    </div>
+    <MobileTable class="mobile-view" :cryptos="state.cryptos" />
     <!-- Pagination -->
     <div class="text-center">
       <v-container>
@@ -105,7 +35,13 @@
 <script>
 import axios from 'axios'
 import { onBeforeMount, reactive, watchEffect } from '@nuxtjs/composition-api'
+import DesktopTable from '~/components/UI/Market/Table/Desktop-Table.vue'
+import MobileTable from '~/components/UI/Market/Table/Mobile-Table.vue'
 export default {
+  components: {
+    DesktopTable,
+    MobileTable,
+  },
   data() {
     return {
       show: false,
@@ -136,14 +72,6 @@ export default {
       state.cryptos = unique
     }
 
-    // Chart color
-    const redOrGreen = (data) => {
-      if (data >= 0.0) {
-        return '#00FF7F'
-      } else {
-        return '#FF4500'
-      }
-    }
     onBeforeMount(getPriceAPI)
     // const handleScrolledToBottom = (isVisible) => {
     //   if (!isVisible) {
@@ -160,7 +88,6 @@ export default {
     return {
       getPriceAPI,
       state,
-      redOrGreen,
       pagination,
     }
   },
@@ -172,7 +99,6 @@ export default {
   height: 100%;
   width: 100%;
 }
-.crypto-data,
 .titles {
   display: grid;
   grid-template-columns: 6% 14% 11% 12% 12% 20% 25%;
@@ -203,8 +129,7 @@ span.crypto-chart {
   display: block;
   text-align: center;
 }
-.mobile-title,
-.mobile {
+.mobile-view {
   display: none;
 }
 a {
@@ -231,20 +156,13 @@ a {
 .crypto-chart {
   grid-area: chart;
 }
-/* .bottom-detector {
-  margin-top: -60%;
-  border: 3px dotted yellow;
-  height: 200px;
-  position: absolute;
-  z-index: -1;
-} */
+
 @media only screen and (max-width: 1080px) {
   .titles,
-  .desktop-layout {
+  .desktop-view {
     display: none;
   }
-  .mobile-title,
-  .mobile {
+  .mobile-view {
     display: block;
   }
   .mobile {
