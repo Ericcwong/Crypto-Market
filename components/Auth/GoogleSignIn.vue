c754662a5ba630a6cafa45cff360c32766c63b2c
Eric Wong <ericwong12@live.com>
2021-07-02 16:04:38 -0600

    Fix: GoogleSignIn.vue
    Moved sign in to auth actions

--- components/Auth/GoogleSignIn.vue
+++ components/Auth/GoogleSignIn.vue
@@ -1,50 +1,25 @@
 <template>
-  <b-dropdown-item @click.prevent="googleSignup">
-    Continue with: &nbsp;
-    <img class="google-icon" src="search.png" alt="Google Icon" />
-  </b-dropdown-item>
+  <v-btn>
+    <img
+      @click.prevent="googleSignup"
+      class="google-icon"
+      src="search.png"
+      alt="Google Icon"
+  /></v-btn>
 </template>
 
 <script>
-import firebase from 'firebase/app'
 export default {
   name: 'GoogleSignIn',
   methods: {
     googleSignup() {
-      var provider = new firebase.auth.GoogleAuthProvider()
-      this.$fire.auth
-        .signInWithPopup(provider)
-        .then((result) => {
-          /** @type {firebase.auth.OAuthCredential} */
-          var credential = result.credential
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = credential.accessToken
-          console.log(token)
-          // The signed-in user info.
-          var user = result.user
-          console.log(user)
-          // ...
-        })
-        .catch((error) => {
-          // Handle Errors here.
-          var errorCode = error.code
-          var errorMessage = error.message
-          // The email of the user's account used.
-          var email = error.email
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential
-          // ...
-        })
+      this.$store.dispatch('auth/googleSignup')
     },
   },
 }
 </script>
 
 <style scoped>
-.button {
-  display: flex;
-  align-items: center;
-}
 .google-icon {
   width: 17px;
 }
