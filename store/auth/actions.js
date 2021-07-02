a7a913bb36b6d77e732509163ca61054b9a5a257
Eric Wong <ericwong12@live.com>
2021-07-02 16:03:40 -0600

    Feat: Auth/actions
    Moved Google sign in option to action
    Favorites will have a way to check if a user would like to create an account

--- store/auth/actions.js
+++ store/auth/actions.js
@@ -1,14 +1,45 @@
+import firebase from 'firebase/app'
+
 const actions = {
-    logoutUser(){
-        this.$fire.auth.signOut().then(() => {
-            // Sign-out successful.
-            console.log("User Signed out")
-            this.$router.push("/")
-          }).catch((error) => {
-            // An error happened.
-            console.log("Sign out Error: " + error)
-          });
-    }
+  logoutUser() {
+    this.$fire.auth
+      .signOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log('User Signed out')
+        this.$router.push('/')
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log('Sign out Error: ' + error)
+      })
+  },
+  googleSignup() {
+    let provider = new firebase.auth.GoogleAuthProvider()
+    this.$fire.auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        let credential = result.credential
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        let token = credential.accessToken
+        console.log(token)
+        // The signed-in user info.
+        let user = result.user
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        let errorCode = error.code
+        let errorMessage = error.message
+        // The email of the user's account used.
+        let email = error.email
+        // The firebase.auth.AuthCredential type that was used.
+        let credential = error.credential
+        // ...
+      })
+  },
 }
 
 export default actions
